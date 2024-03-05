affa11a41b0815bf3200b1db9792e602727be932
Emrah IŞIK <ssemrah@outlook.com>
2024-03-05 17:09:58 +0300

    05/03/2024

---
+++ odev/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(title: Text("merhaba ilk uygulamam")),
+      body: Center(
+        child: Text('orta kısım'),
+      ),
+    ),
+  ));
+}
