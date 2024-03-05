68016bad9acfaad7cc581062ebeae089461347f8
Emrah IŞIK <ssemrah@outlook.com>
2024-03-05 22:15:16 +0300

    05/03/2024

--- odev/lib/main.dart
+++ odev/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(MaterialApp(home: Myapp()));
@@ -8,29 +9,40 @@ class Myapp extends StatelessWidget {
   String mesaj = 'Merhaba İlk uygulamam';
   @override
   Widget build(BuildContext context) {
+    var ogrenciler = ["engin demiroğ", 'Kerem Variş', "Berkya bilgin", "murat kurtboğan"];
     return Scaffold(
       appBar: AppBar(title: Text(mesaj)),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            int puan = 55;
-            String mesaj = "";
-            if (puan >= 50) {
-              mesaj = 'Geçti';
-            } else if (puan >= 40) {
-              mesaj = 'Bütünlemem';
-            } else {
-              mesaj = 'Kaldi';
-            }
-            var alert = AlertDialog(
-              title: Text('Sinav Sonucu'),
-              content: Text(mesaj),
-            );
+      body: Column(
+        children: [
+          Expanded(
+              child: ListView.builder(
+                  itemCount: ogrenciler.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Text(ogrenciler[index]);
+                  })),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                int puan = 55;
+                String mesaj = "";
+                if (puan >= 50) {
+                  mesaj = 'Geçti';
+                } else if (puan >= 40) {
+                  mesaj = 'Bütünlemem';
+                } else {
+                  mesaj = 'Kaldi';
+                }
+                var alert = AlertDialog(
+                  title: Text('Sinav Sonucu'),
+                  content: Text(mesaj),
+                );
 
-            showDialog(context: context, builder: (BuildContext context) => alert);
-          },
-          child: Text('sonucu gör'),
-        ),
+                showDialog(context: context, builder: (BuildContext context) => alert);
+              },
+              child: Text('sonucu gör'),
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ odev1/map.dart
@@ -0,0 +1,22 @@
+void main() {
+  //map
+
+  var dictionary1 = new Map();
+  dictionary1["book"] = "kitap";
+  dictionary1["little"] = "küçük";
+  var dictionary2 = {"kitap": "book", "küçük": "little"};
+
+  print(dictionary1);
+  print(dictionary2);
+
+  for (var key in dictionary2.keys) {
+    print(key);
+  }
+  for (var value in dictionary2.values) {
+    print(value);
+  }
+
+  print(dictionary2.containsKey("kitap"));
+
+  dictionary2.forEach((k, v) => {print(k + " : " + v)});
+}
