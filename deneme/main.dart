9e60ed46670f65416b2996195d5da72376ade819
Emrah IŞIK <ssemrah@outlook.com>
2024-03-12 09:16:43 +0300

    121

---
+++ deneme/main.dart
@@ -0,0 +1,7 @@
+import 'user.dart';
+
+void main() {
+  User user1 = new User(name: "Emrah", sayi: 5);
+
+  user1.display();
+}
---
+++ deneme/user.dart
@@ -0,0 +1,9 @@
+class User {
+  User({required this.name, required this.sayi}) {}
+  String name;
+  int sayi;
+
+  void display() {
+    print("isim: $name sayi:$sayi");
+  }
+}
