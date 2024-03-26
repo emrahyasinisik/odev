e0dd1a620e895027c42b0eb84aeb51dbaa3b191a
Emrah IŞIK <ssemrah@outlook.com>
2024-03-26 23:06:20 +0300

    Updated

--- odev4-mealsapp/lib/screens/categories.dart
+++ odev4-mealsapp/lib/screens/categories.dart
@@ -1,29 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:odev4/data/category_data.dart';
 import 'package:odev4/models/category.dart';
+import 'package:odev4/screens/meals.dart';
 import 'package:odev4/widgets/category_card.dart';
 
+// Global State - Global State Management
 class Categories extends StatelessWidget {
   const Categories({super.key});
 
+  void _changeScreen(BuildContext context, Category c) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => Meals(category: c),
+      ),
+    );
+  }
+
+  // * => Context objesi StatlessWidgetlarda yalnızca build fonksiyonu içerisinde erişime açıktır.
+  // => Stateful Widgetlarda ise, build fonksiyonu dışından da erişilebilir.
   @override
   Widget build(BuildContext context) {
-    List<Category> categoryList = categories;
+    const List<Category> categoryList = categories;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Kategoriler"),
+        actions: [
+          // TODO: Favoriler sayfasına geçiş..
+          IconButton(onPressed: () {}, icon: const Icon(Icons.favorite))
+        ],
       ),
       body: GridView(
         padding: const EdgeInsets.all(8),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 8,
-            childAspectRatio: 1 / 1),
+            crossAxisSpacing: 25,
+            mainAxisSpacing: 5,
+            childAspectRatio: 4 / 2),
         children: categoryList
             .map((e) => CategoryCard(
-                  name: e.name,
-                  color: e.color,
+                  category: e,
+                  onCategoryClick: () {
+                    //Navigator.of(context)
+                    //Theme.of(context)
+                    //Localizations.of(context)
+                    _changeScreen(context, e);
+                  },
                 ))
             .toList(),
       ),
---
+++ odev4-mealsapp/lib/screens/meals.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:odev4/data/meal_data.dart';
+import 'package:odev4/models/category.dart';
+import 'package:odev4/models/meal.dart';
+import 'package:odev4/widgets/meal_card.dart';
+
+class Meals extends StatelessWidget {
+  const Meals({super.key, required this.category});
+  final Category category;
+  @override
+  Widget build(BuildContext context) {
+    List<Meal> mealList =
+        meals.where((element) => element.categoryId == category.id).toList();
+
+    // ListView
+    Widget widget = ListView.builder(
+      itemBuilder: (context, index) => MealCard(
+        meal: mealList[index],
+      ),
+      itemCount: mealList.length,
+    );
+
+    if (mealList.isEmpty) {
+      widget = const Center(
+        child: Text("Bu kategoride hiç bir yemek bulunamadı."),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(title: Text("${category.name} Yemekleri")),
+      body: widget,
+    );
+  }
+}
