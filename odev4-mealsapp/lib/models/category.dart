8f04984f673ee1df74beb152f54e391c10d4d45f
Şule Duman <sduman_816@hotmail.com>
2024-03-30 18:12:17 +0300

    Added ekremtek_part

--- odev4-mealsapp/lib/models/category.dart
+++ odev4-mealsapp/lib/models/category.dart
@@ -1,9 +1,7 @@
-import 'package:flutter/material.dart';
-
 class Category {
   const Category(
-      {required this.id, required this.name, this.color = Colors.orange});
+      {required this.id, required this.name, required this.imageUrl});
   final String id;
   final String name;
-  final Color color;
+  final String imageUrl;
 }
