4092f52e983d1fc1ca2ba70484f2c75dd9b1d760
ekremtek <ekremtek@gmail.com>
2024-03-30 16:06:24 +0300

    ekremtek30032024

--- odev4-mealsapp/lib/screens/categories.dart
+++ odev4-mealsapp/lib/screens/categories.dart
@@ -25,23 +25,60 @@ class Categories extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Kategoriler"),
+        centerTitle: true, // Başlığı ortala
+        title: Container(
+          padding: const EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            color: Colors.amberAccent, // Çerçeve iç rengi
+            border: Border.all(
+              color: Colors.black, // Çerçeve rengi
+              width: 2, // Çerçeve kalınlığı
+            ),
+            borderRadius:
+                BorderRadius.circular(12), // Çerçeve kenar yuvarlaklığı
+          ),
+          child: const Text(
+            "Restaurant Menu",
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.black, // Yazı rengi
+            ),
+          ),
+        ),
         actions: [
           IconButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (ctx) => const Favorites()));
-              },
-              icon: const Icon(Icons.favorite))
+            onPressed: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (ctx) => const Favorites()));
+            },
+            icon: const Stack(
+              alignment: Alignment.center,
+              children: [
+                // Dış çerçeve (kalp simgesi)
+                Icon(
+                  Icons.favorite_border,
+                  color: Colors.black, // Dış çerçeve rengi siyah
+                  size: 30, // Dış çerçevenin boyutu
+                ),
+                // İç kısım (kalp simgesi)
+                Icon(
+                  Icons.favorite,
+                  color: Colors.amberAccent, // İç kısım rengi mor
+                  size: 25, // İç kısmın boyutu
+                ),
+              ],
+            ),
+          )
         ],
       ),
       body: GridView(
         padding: const EdgeInsets.all(8),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
-            crossAxisSpacing: 25,
-            mainAxisSpacing: 5,
-            childAspectRatio: 4 / 2),
+            crossAxisSpacing: 50,
+            mainAxisSpacing: 50,
+            childAspectRatio: 1),
         children: categoryList
             .map((e) => CategoryCard(
                   category: e,
--- odev4-mealsapp/lib/widgets/category_card.dart
+++ odev4-mealsapp/lib/widgets/category_card.dart
@@ -2,27 +2,49 @@ import 'package:flutter/material.dart';
 import 'package:odev4/models/category.dart';
 
 class CategoryCard extends StatelessWidget {
-  const CategoryCard(
-      {super.key, required this.category, required this.onCategoryClick});
+  const CategoryCard({
+    super.key,
+    required this.category,
+    required this.onCategoryClick,
+  });
+
   final Category category;
   final void Function() onCategoryClick;
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      // onTap: onCategoryClick, -> Sadece dışarıyı haberdar et;
       onTap: () {
-        onCategoryClick(); // Dışarıyı haberdar et
-        // Varsa diğer işlemleri yürüt.
+        onCategoryClick();
       },
       child: Container(
         decoration: BoxDecoration(
-            gradient: LinearGradient(colors: [
-              category.color.withOpacity(0.5),
-              category.color.withOpacity(0.9)
-            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-            borderRadius: BorderRadius.circular(16)),
-        padding: const EdgeInsets.all(16.0),
-        child: Text(category.name),
+          borderRadius: BorderRadius.circular(100),
+          image: DecorationImage(
+            image: NetworkImage(
+                category.imageUrl), // Resim URL'si burada kullanılıyor
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              color: Colors.black.withOpacity(0.6),
+              padding: const EdgeInsets.all(1),
+              child: Text(
+                category.name,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
